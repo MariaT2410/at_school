be0b02b15074c8f7485baa6712d5f3b2c230e1ab
29_06 <29_06_titova@mail.ru>
2023-05-05 19:59:53 +0300

    коммит 8 для ветки homework_unit_testing

--- src/test/java/NegativeCalculatorTest.java
+++ src/test/java/NegativeCalculatorTest.java
@@ -12,29 +12,31 @@ public class NegativeCalculatorTest {
                 {"+", "-2147483648 ", "7.2"},
                 {"-", "", "6.0"},
                 {"*", "one", "2.0"},
-                {"/", "10.0", "0"}
+                {"/", "10.0", "0"},
+                {"+", "-2147483649 ", "7.2"},
+                {"", "10.0", "0"},
         };
     }
-    @Test(dataProvider = "negativeData", expectedExceptions= {ArithmeticException.class, NullPointerException.class, NumberFormatException.class})
+    @Test(dataProvider = "negativeData", expectedExceptions= {RuntimeException.class})
     public void negativeTest( String op, String n1, String n2)
     {
-        if(op.equals("")){throw new NullPointerException();}
+        if(op.equals("")){throw new RuntimeException();}
         else {
             if (!op.isEmpty() && !n1.isEmpty() && !n2.isEmpty()) {
                 //деление на 0 и пустые строки
                 if ((n2.equals("0")) && (op.equals("/"))) {
-                    throw new ArithmeticException("Деление на 0");
+                    throw new RuntimeException("Деление на 0");
                 } else {
                     try {
                         Double.parseDouble(n1);
                         Double.parseDouble(n2);
-                    } catch (NumberFormatException e) { }
+                    } catch (RuntimeException e) { }
                     if (Double.parseDouble(n1) < Integer.MAX_VALUE || Double.parseDouble(n1) > Integer.MIN_VALUE || Double.parseDouble(n2) < Integer.MAX_VALUE || Double.parseDouble(n2) < Integer.MIN_VALUE) {
-                        throw new ArithmeticException("Одно из чисел вне границ диапазона");
+                        throw new RuntimeException("Одно из чисел вне границ диапазона");
                     }
                 }
             } else {
-                throw new NullPointerException();
+                throw new RuntimeException();
             }
         }
     }
