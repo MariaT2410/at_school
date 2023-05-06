2659e200e5055c5a28f40f554e5d680c175c1453
29_06 <29_06_titova@mail.ru>
2023-05-06 22:49:56 +0300

    коммит 22 для ветки homework_unit_testing

--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -11,15 +11,13 @@ public class Calculator {
         //Calculator.isOperator(params);
         String operator = params[isOperator(params)];
         try {
-            if(isNum(params).get(0)!=null||isNum(params).get(1)!=null) {
-                double value1 = Double.parseDouble(isNum(params).get(0));
-                double value2 = Double.parseDouble(isNum(params).get(1));
+                double value1 = Double.parseDouble(isNum(params)[0]);
+                double value2 = Double.parseDouble(isNum(params)[1]);
                 double result = Math.round(calculate(operator, value1, value2) * 1000.0) / 1000.0;
                 if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE || value1 > Integer.MAX_VALUE || value1 < Integer.MIN_VALUE || value2 > Integer.MAX_VALUE || value2 < Integer.MIN_VALUE) {
                     throw new CalculatorException("Превышен порог у вводимых значений и выводимых значений");
                 }
                 return String.valueOf(result);
-            }else throw new CalculatorException("Index: 0, Size: 0");
         } catch (NumberFormatException e){
             throw new CalculatorException("Проблема перевода строки в число");
         }
@@ -76,16 +74,20 @@ public class Calculator {
         }
         return newParam;
     }
-    private static ArrayList<String> isNum(String[] params){
+    private static String[] isNum(String[] params){
         //String[] newParam = new String[]{};
         ArrayList<String> res = new ArrayList<>();
+        String[] resul = new String[2];
         String a;
+        int j=0;
         for(String i:params) {
             if(!i.equals(params[isOperator(params)])){
                 res.add(i);
+                resul[j]=i;
+                j++;
             }
         }
-        return res;
+        return resul;
     }
 
 }
